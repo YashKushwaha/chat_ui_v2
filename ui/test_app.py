a547043e10ae45193317f7e48ab526425dee69df
Yash Kushwaha <yash1158@gmail.com>
2025-05-15 15:35:19 +0530

    simple working fast API app created

---
+++ ui/test_app.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    import uvicorn
+    app_path = Path(__file__).resolve().with_suffix('').name  # gets filename without .py
+    uvicorn.run(f"{app_path}:app", host="0.0.0.0", port=8000, reload=True)
